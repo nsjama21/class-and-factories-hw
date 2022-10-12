1d18e9a474c8ae8c53d76f04ff6173480bb34336
nsjama15 <nsjama21@gmail.com>
2022-10-12 19:55:00 -0400

    created person class

--- class-and-factories.js
+++ class-and-factories.js
@@ -1,8 +1,8 @@
 class Hamster {
-    constructor(ownerParam, priceParam) {
+    constructor(priceParam) {
         this.owner = ""
         this.name = "BillyBob"
-        this.price = priceParam
+        this.price = 15
     }
 
     wheelRun() {
@@ -14,8 +14,63 @@ class Hamster {
     }
 
     getPrice() {
-        return
+        return this.price
     }
 
+}
+
+class Person {
+    constructor(personNameParam) {
+        this.personName = personNameParam
+        this.age = 0
+        this.height = 0
+        this.weight = 0
+        this.mood = 0
+        this.hamsters = []
+        this.bankAccount = 0
+
+    }
+
+    getName() {
+        return this.personName
+
+    }
+
+    getAge() {
+        return this.age
+
+    }
+
+    getWeight() {
+        return this.weight
+    }
+
+    greet() {
+        console.log(`Hello, I am ${this.name}!`)
+    }
+
+    eat() {
+        this.weight++
+        this.mood++
+    }
+
+    exercise() {
+        this.weight--
+    }
+
+    ageUp() {
+        this.age++
+        this.height++
+        this.weight++
+        this.mood--
+        this.bankAccount += 10
+    }
+
+    buyHamster(hamster) {
+        this.hamsters.push(hamster)
+        this.mood += 10
+        this.bankAccount -= getPrice()
+    }
 
 }
+
