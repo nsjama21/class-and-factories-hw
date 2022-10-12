22b687778da0ea92f69898976effe5021cf54d46
nsjama15 <nsjama21@gmail.com>
2022-10-12 19:10:18 -0400

    created hamster class

---
+++ class-and-factories.js
@@ -0,0 +1,21 @@
+class Hamster {
+    constructor(ownerParam, priceParam) {
+        this.owner = ""
+        this.name = "BillyBob"
+        this.price = priceParam
+    }
+
+    wheelRun() {
+        console.log("squeak squeak")
+    }
+
+    eatFood() {
+        console.log("nibble nibble")
+    }
+
+    getPrice() {
+        return
+    }
+
+
+}
